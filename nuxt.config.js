53c0bd561cf5f9deba3183501ff043f962fb33a1
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-03 17:54:12 +0100

    made changes: implementing auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,12 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxtjs/auth-next'
   ],
+
+  router: {
+    middleware: ['auth']
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,6 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+ 
 }
 </script>
