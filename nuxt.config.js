e5a5dfa4c765784e9520b249acb225206b85ebae
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-03 18:28:53 +0100

    made changes: implementing auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,9 +58,9 @@ export default {
           // autoFetch: true
         },
         endpoints: {
-          login: { url: '/api/auth/login', method: 'post' },
-          logout: { url: '/api/auth/logout', method: 'post' },
-          user: { url: '/api/auth/user', method: 'get' }
+          login: { url: 'http://localhost:3000/api/auth/login', method: 'post' },
+          logout: { url: 'http://localhost:3000/api/auth/logout', method: 'post' },
+          user: { url: 'http://localhost:3000/api/auth/user', method: 'get' }
         }
       }
     }
