2b6b661d4fffa873447aea2d0f2826179ee52fbc
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-04 11:41:06 +0100

    created register page

--- pages/register.vue
+++ pages/register.vue
@@ -1,13 +1,19 @@
 <template>
-  
+  <div class="container">
+    <h1>Signup for JTP todos</h1>
+  </div>
 </template>
 
 <script>
 export default {
-
+  auth: 'guest'
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.container {
+  h1 {
+    @include display-1;
+  }
+}
 </style>
