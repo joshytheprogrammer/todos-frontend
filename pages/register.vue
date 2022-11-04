830353ce4bee27bce1087e5c542749179e8b70fc
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-04 11:44:34 +0100

    improving register page

--- pages/register.vue
+++ pages/register.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="container">
     <h1>Signup for JTP todos</h1>
+    <form @submit.prevent="register">
+      <div class="form-group">
+        <label>Username</label>
+        <input type="text" v-model="register.username" placeholder="Enter your username" />
+      </div>
+      <div class="form-group">
+        <label>Email</label>
+        <input type="text" v-model="register.email" placeholder="Enter your email" />
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" v-model="register.password" placeholder="Enter your password" />
+      </div>
+      <div class="submit-line">
+        <button type="submit">Register</button>
+        <p> Already have an account <NuxtLink to="/register">Login</NuxtLink></p>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  auth: 'guest'
+  auth: 'guest',
+  data() {
+    return {
+      register: {
+        username: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    register() {
+
+    }
+  }
 }
 </script>
 
