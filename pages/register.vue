645d85996dad4bd7b256daa4655b56be820bacba
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-04 11:41:53 +0100

    improving register page

--- pages/register.vue
+++ pages/register.vue
@@ -12,6 +12,22 @@ export default {
 
 <style lang="scss" scoped>
 .container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  width: 400px;
+  max-width: 90%;
+
+  height: fit-content;
+
+  margin: 2rem auto;
+  padding: 2rem 1rem;
+
+  border: 1px solid $dark;
+  box-shadow: 0px 4px 4px $dark;
+  border-radius: 10px;
+
   h1 {
     @include display-1;
   }
