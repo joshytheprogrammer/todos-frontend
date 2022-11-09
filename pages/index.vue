0bc598fbbfe021abb049e9162c226f8284d6e495
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-09 16:38:08 +0100

    made changes: ''

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="dashboard">
+    <Nav />
     <SearchBar />
     <PageTitle :numOftasks="'4'" />
   </div>
 </template>
 
 <script>
+import Nav from "../components/App/Nav.vue"
 import SearchBar from "../components/Home/Bar.vue"
 import PageTitle from "../components/Home/Heading.vue"
 export default {
   auth: 'guest',
   components: {
+    Nav,
     SearchBar,
     PageTitle
   }
