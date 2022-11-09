117594b472b3e7c97cb061d6d0de5d6c5d46bbf0
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-09 16:42:11 +0100

    made changes: ''

--- components/App/Nav.vue
+++ components/App/Nav.vue
@@ -15,5 +15,14 @@ export default {
 .nav {
   display: flex;
   justify-content: space-between;
+  padding: 14px;
+
+  h2 {
+    @include page-title;
+  }
+
+  button {
+    @include btn;
+  }
 }
 </style>
