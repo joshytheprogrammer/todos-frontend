3d5f04375c836a68a9bd8ab8c312d04b853f5f71
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-11 10:41:51 +0100

    made changes

---
+++ components/App/Create.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="create">
+    <Modal>
+      <template #title>
+        <h1>Create new task</h1>
+      </template>
+      <template #body>
+
+      </template>
+
+    </Modal>
+  </div>
+</template>
+
+<script>
+import Modal from "./Modal.vue"
+export default {
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style>
+
+</style>
