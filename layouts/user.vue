8c387dfcb3ec945364abb78f91f8ab76b99104a2
joshytheprogrammer <jurassicjoshua@protonmail.com>
2022-11-16 09:51:26 +0100

    revamping site

--- layouts/user.vue
+++ layouts/user.vue
@@ -2,9 +2,7 @@
   <div class="app">
     <Nav />
     <SearchBar />
-    <Transition name="fade">
-      <Create v-show="showingCreate" />
-    </Transition>
+
     <Nuxt />
   </div>
 </template>
@@ -12,31 +10,11 @@
 <script>
 import Nav from "../components/App/Nav.vue"
 import SearchBar from "../components/App/Bar.vue"
-import Create from "../components/App/Create.vue"
 
-import { mapGetters } from 'vuex'
 export default {
-  computed: {
-    ...mapGetters({
-      showingCreate: 'isShowingCreateModal'
-    })
-  },
   components: {
     Nav,
     SearchBar,
-    Create
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
--- pages/create.vue
+++ pages/create.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="create">
-    <h1>Create new task</h1>
-    <form @submit.prevent="submitTask">
-      <div class="form-group">
-        <label for="title">Title</label>
-        <input type="text" name="title" v-model="task.title" placeholder="What would you call this task?" required>
-      </div>
-      <div class="form-group">
-        <label for="desc">Description</label>
-        <textarea type="text" name="desc" v-model="task.desc" placeholder="How would you describe it?" required></textarea>
-      </div>
-      <div class="form-group">
-        <label for="deadline">Deadline</label>
-        <input type="datetime-local" name="deadline" v-model="task.date" placeholder="When does the task end?" required>
-      </div>
-      <div class="submit">
-        <button type="submit">Create task</button>
-      </div>
-    </form>
+  <div class="container">
+    <div class="create">
+      <h1>Create new task</h1>
+      <form @submit.prevent="submitTask">
+        <div class="form-group">
+          <label for="title">Title</label>
+          <input type="text" name="title" v-model="task.title" placeholder="What would you call this task?" required>
+        </div>
+        <div class="form-group">
+          <label for="desc">Description</label>
+          <textarea type="text" name="desc" v-model="task.desc" placeholder="How would you describe it?" required></textarea>
+        </div>
+        <div class="form-group">
+          <label for="deadline">Deadline</label>
+          <input type="datetime-local" name="deadline" v-model="task.date" placeholder="When does the task end?" required>
+        </div>
+        <div class="submit">
+          <button type="submit">Create task</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -43,66 +45,68 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.create {
+.container {
+  .create {
 
-  h1 {
-    @include page-title;
-  }
-
-  form {
-    display: block;
-    padding: 1rem 0;
-
-    .form-group {
-      display: block;
-      padding-top: 1rem;
-
-      label {
-        display: inline-block;
-        @include label;
-        font-size: 16px
-      }
-
-      input {
-        @include form-control;
-        font-size: 14px;
-      }
-
-      textarea {
-        @include form-control;
-        font-size: 14px;
-        height: 180px;
-        padding: 0.8rem 0.4rem;
-
-        &::-webkit-resizer {
-          display: none;
-        }
-
-        &::-webkit-scrollbar {
-          width: 8px;
-          height: 4px;
-        }
-
-        &::-webkit-scrollbar-track {
-          background: $light;
-        }
-
-        &::-webkit-scrollbar-thumb {
-          background: $primary;
-        }
-
-        &::-webkit-scrollbar-thumb:hover {
-          background: $dark;
-        }
-      }
+    h1 {
+      @include page-title;
     }
 
-    .submit {
-      padding-top: 2rem;
+    form {
+      display: block;
+      padding: 1rem 0;
 
-      button {
-        @include btn;
-        padding: 1rem 2rem;
+      .form-group {
+        display: block;
+        padding-top: 1rem;
+
+        label {
+          display: inline-block;
+          @include label;
+          font-size: 16px
+        }
+
+        input {
+          @include form-control;
+          font-size: 14px;
+        }
+
+        textarea {
+          @include form-control;
+          font-size: 14px;
+          height: 180px;
+          padding: 0.8rem 0.4rem;
+
+          &::-webkit-resizer {
+            display: none;
+          }
+
+          &::-webkit-scrollbar {
+            width: 8px;
+            height: 4px;
+          }
+
+          &::-webkit-scrollbar-track {
+            background: $light;
+          }
+
+          &::-webkit-scrollbar-thumb {
+            background: $primary;
+          }
+
+          &::-webkit-scrollbar-thumb:hover {
+            background: $dark;
+          }
+        }
+      }
+
+      .submit {
+        padding-top: 2rem;
+
+        button {
+          @include btn;
+          padding: 1rem 2rem;
+        }
       }
     }
   }
